8d2a1e9c6d4c4815aac596491ccb3c1d24182ff4
Rudraksh Srivsatva <rudraksh2805@gmail.com>
2024-02-29 21:20:37 +0530

    Worked on the homepage, finishing the header and working on the 'hero' contant. Added a file to contain the sample images for the initial phase of the project.

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,28 @@
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
     <div id="homepage-header">
       <Header />
+      <div id="homepage-call-to-act">
+        <div id="homepage-head-content">
+          <h3 id="homepage-roboto-heading">
+            WrapZip Clothing Line
+          </h3>
+          <p id="homepage-large-font">
+            You can explore our high quality and<br/> fashionable clothing line!
+          </p>
+          <button>
+            Shop Now
+          </button>
+        </div>
+        <div id="homepage-image-content">
+
+        </div>
+      </div>
       <Footer />
     </div>
     </>
