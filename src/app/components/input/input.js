826ed3b6fcaf60e7059153b1822318a1ffdc1f19
Rudraksh Srivsatva <rudraksh2805@gmail.com>
2024-08-22 01:02:33 +0100

    -

--- src/app/components/input/input.js
+++
@@ -1,9 +0,0 @@
-import './input.css';
-
-export function Input({ type, placeholder }) {
-    return (
-        <div className='input'>
-            <input type={type || 'text'} placeholder={ placeholder || ''} />
-        </div>
-    )
-}
--- src/app/sign-up/page.js
+++
@@ -1,9 +0,0 @@
-function signUp() {
-    return (
-        <div>
-            <h1>Sign Up</h1>
-        </div>
-    )
-}
-
-export default signUp; 
